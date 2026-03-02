b0d0eedf3bb05ed1f8502ed3d86455a5e6e52331
Walt <83287020+wveratudela@users.noreply.github.com>
2026-03-02 10:50:38 +0100

    Updating files

--- Q2_mean_reversion/Q2_functions.py
+++ Q2_mean_reversion/Q2_functions.py
@@ -4,16 +4,6 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 import seaborn as sns
 
-def fetch_data(ticker, start, end):
-
-    df = yf.download(ticker, start=start, end=end, interval="1d", auto_adjust=False, progress=False)
-    df.columns = df.columns.get_level_values(0)
-    df = df[['Close', 'Volume']]
-    df = df.ffill()
-
-    return df
-
-
     
 def add_signals(df):
     
