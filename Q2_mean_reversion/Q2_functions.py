c2a4eef87478c3279071103de70fa7ed7e4eaa8d
Walt <83287020+wveratudela@users.noreply.github.com>
2026-03-05 21:32:32 +0100

    Finished Q2 and updated Q1
    Q2 notebook and functions are now finished and tested.
    Q1 has been cleaned and updated.

--- Q2_mean_reversion/Q2_functions.py
+++ Q2_mean_reversion/Q2_functions.py
@@ -3,220 +3,309 @@ import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
 import seaborn as sns
-
-    
-def add_signals(df):
-    
-    df = df.copy()
-    
-    df['Log_Returns'] = np.log(df['Close'] / df['Close'].shift(1))
-    df['Daily_Return'] = df['Close'].pct_change()
-    df = df.dropna()
-
-    # Calculate 20-day and 50-day rolling averages
-    df['MA20'] = df['Close'].rolling(window=20).mean()
-    df['MA50'] = df['Close'].rolling(window=50).mean()
-    
-    # Generate signals based on crossovers
-    # Signal = 1 (long) when 20-day crosses above 50-day - Crossover UP
-    # Signal = 0 (flat/cash) when 20-day crosses below 50-day - Crossover DOWN
-    df['Signal'] = (df['MA20'] > df['MA50']).astype(int)
-    
-    # Identify crossover points
-    df['Golden_Cross'] = (df['Signal'] == 1) & (df['Signal'].shift(1) == 0)  # MA20 crosses above MA50
-    df['Death_Cross'] = (df['Signal'] == 0) & (df['Signal'].shift(1) == 1)   # MA20 crosses below MA50
-    
-    return df
+from statsmodels.tsa.stattools import adfuller, coint
+from sklearn.linear_model import LinearRegression
 
 
+def check_data(df1, df2):
 
-def run_backtest(df, starting_capital, tranche=False):
+    result = adfuller(df1['Close'])
+    print(f"V ADF: {result[0]:.4f}, p-value: {result[1]:.4f}")
+    if result[1] < 0.05:
+        print(f"- - - - - WARNING: p-value: {result[1]:.4f} < 5% - - - - -")
+    
+    result = adfuller(df2['Close'])
+    print(f"MA ADF: {result[0]:.4f}, p-value: {result[1]:.4f}")
+    if result[1] < 0.05:
+        print(f"- - - - - WARNING: p-value: {result[1]:.4f} < 5% - - - - -")
+    
+    score, p_value, critical_values = coint(df1['Close'], df2['Close'])
+    print(f"Cointegration p-value: {p_value:.4f}")
+    if p_value > 0.05:
+        print(f"- - - - - WARNING: p-value: {p_value:.4f} > 5% - - - - -")
+    print(f"Critical values: {critical_values}")
+    
 
-    df = df.copy()
+def check_signals(df1, df2):
+
+    # Regress V on MA
+    X = df2['Close'].values.reshape(-1, 1)
+    y = df1['Close'].values
     
-    # Create a column for the current day's relationship between MA20 and MA50
-    df['Cash'] = 0.0
-    df['Portfolio_Value'] = 0.0
-    df['Shares'] = 0.0
+    model = LinearRegression().fit(X, y)
+    beta = model.coef_[0]
+    print(f"Hedge ratio β: {beta:.4f}")
     
+    r2 = model.score(X, y)
+    print(f"R²: {r2:.4f}")
+    if r2 < 0.95:
+        print(f"- - - - - WARNING: p-value: {p_value:.4f} < 95% - - - - -")
+
+    x_line = np.linspace(X.min(), X.max(), 200).reshape(-1, 1)
+    y_line = model.predict(x_line)
+    
+    spread = df1['Close'] - beta * df2['Close']
+
+    print(f"Spread mean: {spread.mean():.4f}")
+    print(f"Spread std: {spread.std():.4f}")
+
+
+    # Visualize the signals to check for correlation and spread
+    plt.figure(figsize=(18, 6))
+    colors = sns.color_palette("colorblind")
+    
+    plt.subplot(1, 2, 1)
+    plt.scatter(X, y, color=colors[0], label='Prices')      # Scatter of original data
+    plt.plot(x_line, y_line, color='k', linewidth=2, label='Fit',linestyle='--')      # Regression line
+    plt.title('MA & V correlation of values')
+    plt.xlabel('MA')
+    plt.ylabel('V')
+    plt.grid(True, alpha=0.3)
+    plt.legend()
+    
+    plt.subplot(1, 2, 2)
+    plt.plot(spread, linewidth=1)
+    plt.axhline(spread.mean(), color='black', linewidth=0.8, linestyle='--', alpha=0.75, label='Mean')
+    plt.axhline(spread.mean()+spread.std(), color='black', linewidth=1, linestyle='--', alpha=0.5, label='+1 std', )
+    plt.axhline(spread.mean()-spread.std(), color='black', linewidth=1, linestyle='--', alpha=0.5, label='-1 std')
+    plt.title('V - 0.55 × MA Spread')
+    plt.xlabel('Year')
+    plt.ylabel('Spread')
+    plt.legend()
+    plt.grid(True, alpha=0.3)
+
+    plt.show()
+
+    
+    rolling_mean = spread.rolling(window=252).mean()
+    rolling_std = spread.rolling(window=60).std()
+    z_score = (spread - rolling_mean) / rolling_std
+    
+    s = z_score
+    signal = pd.Series(0, index=s.index)
+    
+    state = 0
+    
+    for i in range(1, len(s)):
+        if state == 0:
+            if s.iloc[i] > 2:
+                state = 1
+            elif s.iloc[i] < -2:
+                state = -1
+    
+        elif state == 1 and s.iloc[i] < 0:
+            state = 0
+    
+        elif state == -1 and s.iloc[i] > 0:
+            state = 0
+    
+        signal.iloc[i] = state
+    
+    plt.figure(figsize=(18, 6))
+    colors = sns.color_palette("colorblind")
+
+    plt.subplot(1, 2, 1)
+    plt.plot(z_score, linewidth=1, label='Z-score')
+    plt.axhline(2, color='red', linestyle='--', linewidth=1, label='+2')
+    plt.axhline(-2, color='green', linestyle='--', linewidth=1, label='-2')
+    plt.axhline(0, color='black', linestyle='--', linewidth=0.8)
+    plt.title('Z-score of V/MA Spread (60d std, 252d mean)')
+    plt.xlabel('Year')
+    plt.ylabel('z-score')
+    plt.legend()
+    plt.grid(True, alpha=0.3)
+
+    plt.subplot(1, 2, 2)
+    plt.plot(signal)    
+    plt.title('Long/Short signal (1 = Long MA/Short V. -1 = Long V/Short MA)')
+    plt.xlabel('Year')
+    plt.ylabel('Signal')
+    plt.grid(True, alpha=0.3)
+    plt.show()
+
+    return signal
+
+
+def run_pairs_trading(df1, df2, signal, starting_capital):
+
     cash = starting_capital
-    shares = 0.0
-    shares2 = 0.0
+    port_val = 0
+    long_shares = 0
+    short_shares = 0
+    short_proceeds = 0
+    long_leg = 0
+    short_leg = 0
     
-    in_gc_window = False
-    gc_day_counter = 0
+    index = np.arange(len(signal))   # or use your real index
+    df = pd.DataFrame({'Signal': signal}, index=df1.index)
+    df.index = pd.to_datetime(df.index)
     
-    for idx, row in df.iterrows():
-        price = row['Close']
-        port_val = shares * price
-        
-        if row['Golden_Cross']:
-            if tranche:
-                shares = (cash/2) // price
-            else:
-                shares = cash // price
-            port_val = shares * price
-            cash -= port_val
+    long_shares_list = []
+    short_shares_list = []
+    long_leg_list = []
+    short_leg_list = []
+    port_val_list = []
+    cash_list = []
     
-            in_gc_window = True
-            gc_day_counter = 0
+    for i in range(len(signal)):
+        sig = signal.iloc[i]
+        price_1 = df1['Close'].iloc[i]
+        price_2 = df2['Close'].iloc[i]
         
-        if in_gc_window and tranche:
-            gc_day_counter += 1
-            if gc_day_counter == 10:
-                if price > row['MA20'] and price > row['MA50']:
-                    shares2 = cash // price
-                    cash -= shares2 * price
-                    shares += shares2
-                    port_val += shares2 * price
-                    
-                in_gc_window = False
-                gc_day_counter = 0
-        
-        if row['Death_Cross']:
-            cash += shares * price
-            shares = 0
+        if sig == 1 and signal.iloc[i-1] == 0: # long_spread
+            long_shares = (cash/2) // price_2
+            short_shares = (cash/2) // price_1
+    
+            long_leg = long_shares * price_2
+            short_proceeds = short_shares * price_1
+           
+            cash -= long_leg
+            cash += short_proceeds
+            port_val = long_leg + short_proceeds
+    
+        elif sig == 0 and signal.iloc[i-1] == 1: # none - reset
+            long_leg = long_shares * price_2
+            short_leg = short_shares * price_1
+    
+            cash += long_leg
+            cash -= short_leg
+            
+            long_shares = 0
+            short_shares = 0
+            short_proceeds = 0
             port_val = 0
-            in_gc_window = False
-            gc_day_counter = 0
-
-        
-        df.loc[idx, 'Cash'] = cash
-        df.loc[idx, 'Portfolio_Value'] = port_val
-        df.loc[idx, 'Shares'] = shares
     
-    df['Total'] = df['Portfolio_Value'] + df['Cash']
-    df['Buy_Hold'] = (starting_capital / df['Close'].iloc[0]) * df['Close']
+        elif sig == -1 and signal.iloc[i-1] == 0: # long_spread
+            long_shares = (cash/2) // price_1
+            short_shares = (cash/2) // price_2
+    
+            long_leg = long_shares * price_1
+            short_proceeds = short_shares * price_2
+            
+            cash -= long_leg
+            cash += short_proceeds
+            port_val = long_leg + short_proceeds
+    
+        elif sig == 0 and signal.iloc[i-1] == -1: # none - reset
+            long_leg = long_shares * price_1
+            short_leg = short_shares * price_2
+    
+            cash += long_leg
+            cash -= short_leg
+            
+            long_shares = 0
+            short_shares = 0
+            short_proceeds = 0
+            port_val = 0
+    
+        if sig == 1 and signal.iloc[i-1] == 1:  # holding: long MA, short V
+            long_leg = long_shares * price_2
+            short_leg = short_proceeds - short_shares * price_1
+            port_val = long_leg + short_leg
+    
+        elif sig == -1 and signal.iloc[i-1] == -1:  # holding: long V, short MA
+            long_leg = long_shares * price_1
+            short_leg = short_proceeds - short_shares * price_2
+            port_val = long_leg + short_leg
+        else:
+            port_val = 0
+    
+        
+        long_shares_list.append(long_shares)
+        short_shares_list.append(short_shares)
+        long_leg_list.append(long_leg)
+        short_leg_list.append(short_leg)
+        port_val_list.append(port_val)
+        cash_list.append(cash)
+    
+    
+    df['Long_Shares'] = long_shares_list
+    df['Short_Shares'] = short_shares_list
+    df['Long_Leg'] = long_leg_list
+    df['Short_Leg'] = short_leg_list
+    df['Portfolio_Value'] = port_val_list
+    df['Cash'] = cash_list
+    df['Total'] = df['Cash'] + df['Portfolio_Value']
 
     return df
-
-
-
+  
+    
 def compute_metrics(df,starting_capital):
 
     df = df.copy()
-    
+
+    if 'Close' in df.columns:
+        df['Buy_Hold'] = (starting_capital / df['Close'].iloc[0]) * df['Close']
+    else:
+        df['Buy_Hold'] = df['Cash']
+        
     trading_days = 252
     risk_free_rate = 4/100
     rf_daily = risk_free_rate / trading_days
     years = (df.index[-1] - df.index[0]).days / 365.25
-
-    
-    #Buy Sell
-    Total_return_BS = (df.iloc[-1]['Total']-starting_capital)/starting_capital
-    
-    tot_log_returns = np.log(df['Total'] / df['Total'].shift(1))
-    excess_return = tot_log_returns - rf_daily
-    mu = excess_return.mean()
-    sigma = excess_return.std()
-    Sharpe_ratio_BS = (mu / sigma) * np.sqrt(trading_days)
-    
-    df['Running_Max'] = df['Total'].cummax()
-    df['Drawdown_BS'] = df['Total'] / df['Running_Max'] - 1
-    Maximum_drawdown_BS = df['Drawdown_BS'].min()
-
-    start_val = df['Total'].iloc[0]
-    end_val   = df['Total'].iloc[-1]
-    CAGR_BS = (end_val / start_val) ** (1 / years) - 1
-    
-    Calmar_BS = CAGR_BS / Maximum_drawdown_BS
-
-    # Calculate the difference between adjacent elements
-    diff = np.diff(df['Signal'])
-    # Rising edges (0 to 1 transitions) are where diff == 1
-    rising_indices = np.where(diff == 1)[0]
-    # Falling edges (1 to 0 transitions) are where diff == -1
-    falling_indices = np.where(diff == -1)[0]
-    buy = df.iloc[rising_indices+1]['Portfolio_Value'].values
-    sell = df.iloc[falling_indices]['Portfolio_Value'].values
-    
-    if len(sell) == len(buy):
-        gains = sell-buy
-    else:
-        gains = sell-buy[0:-1]
-        print('Last buy still holding.')
-    
-    pos = np.sum(gains > 0)
-    neg = np.sum(gains < 0)
-    tot = len(gains)
-    
-    Win_rate_BS = pos/tot
-
-    df['Year'] = df.index.year
-    yearly_start = df.groupby('Year')['Total'].first()
-    yearly_end   = df.groupby('Year')['Total'].last()
-    days = df.groupby('Year').apply(lambda x: (x.index[-1] - x.index[0]).days,
-    include_groups=False)
-    annualized_BS = (yearly_end / yearly_start) ** (365.25 / days) - 1
-
-
-
-
     
     #Buy Hold
-    Total_return_BH = (df.iloc[-1]['Buy_Hold']-starting_capital)/starting_capital
+    Total_return = (df.iloc[-1]['Buy_Hold']-starting_capital)/starting_capital
     
     tot_log_returns = np.log(df['Buy_Hold'] / df['Buy_Hold'].shift(1))
     excess_return = tot_log_returns - rf_daily
     mu = excess_return.mean()
     sigma = excess_return.std()
-    Sharpe_ratio_BH = (mu / sigma) * np.sqrt(trading_days)
+    Sharpe_ratio = (mu / sigma) * np.sqrt(trading_days)
     
     df['Running_Max'] = df['Buy_Hold'].cummax()
-    df['Drawdown_BH'] = df['Buy_Hold'] / df['Running_Max'] - 1
-    Maximum_drawdown_BH = df['Drawdown_BH'].min()
+    df['Drawdown'] = df['Buy_Hold'] / df['Running_Max'] - 1
+    Maximum_drawdown = df['Drawdown'].min()
 
     start_val = df['Buy_Hold'].iloc[0]
     end_val   = df['Buy_Hold'].iloc[-1]
-    CAGR_BH = (end_val / start_val) ** (1 / years) - 1
+    CAGR = (end_val / start_val) ** (1 / years) - 1
     
-    Calmar_BH = CAGR_BH / Maximum_drawdown_BH
-
-    if Total_return_BH > 0:
-        Win_rate_BH = 1
-    else:
-        Win_rate_BH = 0
+    Calmar = CAGR / Maximum_drawdown
 
     df['Year'] = df.index.year
     yearly_start = df.groupby('Year')['Buy_Hold'].first()
     yearly_end   = df.groupby('Year')['Buy_Hold'].last()
     days = df.groupby('Year').apply(lambda x: (x.index[-1] - x.index[0]).days,
     include_groups=False)
-    annualized_BH = (yearly_end / yearly_start) ** (365.25 / days) - 1
+    annualized = (yearly_end / yearly_start) ** (365.25 / days) - 1
 
     yearly_df = pd.DataFrame({
     'Start': yearly_start,
     'End': yearly_end,
     'Days': days,
-    'Annualized_BS': annualized_BS,
-    'Annualized_BH': annualized_BH
+    'Annualized': annualized
     })
 
+    if 'Close' in df.columns:
+        data = {
+        'Buy Hold': [Total_return, Sharpe_ratio, Maximum_drawdown, CAGR, Calmar]
+        }
+    else:
+        data = {
+        'Pairs': [Total_return, Sharpe_ratio, Maximum_drawdown, CAGR, Calmar]
+        }    
     
     
-    data = {
-        'Buy Sell': [Total_return_BS, Sharpe_ratio_BS, Maximum_drawdown_BS, CAGR_BS, Calmar_BS, Win_rate_BS],
-        'Buy Hold': [Total_return_BH, Sharpe_ratio_BH, Maximum_drawdown_BH, CAGR_BH, Calmar_BH, Win_rate_BH]
-    }
-    
-    index = ['Total Return', 'Sharpe', 'Max Drawdown', 'CAGR', 'Calmar', 'Win Rate']
+    index = ['Total Return', 'Sharpe', 'Max Drawdown', 'CAGR', 'Calmar']
     
     comparison_table = pd.DataFrame(data, index=index)
 
     return df, comparison_table, yearly_df
 
-def plot_performance(dA,dS,yA,yS):
-
     
+def plot_performance(dataV,dataM,dataS,dataP,yV,yM,yS,yP):
 
     plt.figure(figsize=(18, 6))
     colors = sns.color_palette("colorblind")
     
     plt.subplot(1, 3, 1)
     plt.title('Equity curve')
-    plt.plot(dA.index, dA['Total'], label='Total A', linewidth=1, color=colors[0])
-    plt.plot(dA.index, dA['Buy_Hold'], label='HODL A', linewidth=1, color=colors[1])
-    plt.plot(dS.index, dS['Buy_Hold'], label='SPY', linewidth=0.5, color=colors[2])
+    plt.plot(dataV.index, dataV['Buy_Hold'], color=colors[0], label='Buy_Hold V', linewidth=1)
+    plt.plot(dataM.index, dataM['Buy_Hold'], color=colors[1], label='Buy_Hold MA', linewidth=1)
+    plt.plot(dataS.index, dataS['Buy_Hold'], color=colors[2], label='Buy_Hold SPY', linewidth=1)    
+    plt.plot(dataS.index, dataP['Total'], color='k', label='Pairs V/MA', linewidth=1.5)    
+
     plt.grid(True, alpha=0.3)
     plt.xlabel('Date')
     plt.ylabel('Value')
@@ -224,9 +313,11 @@ def plot_performance(dA,dS,yA,yS):
     
     plt.subplot(1, 3, 2)
     plt.title('Drawdown chart')
-    plt.plot(dA.index, dA['Drawdown_BS'], label='Total A', linewidth=1, color=colors[0])
-    plt.plot(dA.index, dA['Drawdown_BH'], label='HODL A', linewidth=1, color=colors[1])
-    plt.plot(dS.index, dS['Drawdown_BH'], label='SPY', linewidth=0.5, color=colors[2])
+    plt.plot(dataV.index, dataV['Drawdown'], color=colors[0], label='Buy_Hold V', linewidth=1)
+    plt.plot(dataM.index, dataM['Drawdown'], color=colors[1], label='Buy_Hold MA', linewidth=1)
+    plt.plot(dataS.index, dataS['Drawdown'], color=colors[2], label='Buy_Hold SPY', linewidth=1)    
+    plt.plot(dataS.index, dataP['Drawdown'], color='k', label='Pairs V/MA', linewidth=1.5)    
+
     plt.grid(True, alpha=0.3)
     plt.xlabel('Date')
     plt.ylabel('Drawdown')
@@ -235,13 +326,14 @@ def plot_performance(dA,dS,yA,yS):
     plt.subplot(1, 3, 3)
     plt.title('Annual returns bar chart')
     
-    years = yA.index
-    x = np.arange(len(years))        # numeric positions for each year
-    width = 0.25                     # width of each bar
+    years = yP.index
+    x = np.arange(len(years))
+    width = 0.2
     
-    plt.bar(x - width, yA['Annualized_BS'], width, label='Total A', color=colors[0])
-    plt.bar(x, yA['Annualized_BH'], width, label='HODL A', color=colors[1])
-    plt.bar(x + width, yS['Annualized_BH'], width, label='SPY', color=colors[2])
+    plt.bar(x - 1.5*width, yV['Annualized'], width, label='V', color=colors[0])
+    plt.bar(x - 0.5*width, yM['Annualized'], width, label='MA', color=colors[1])
+    plt.bar(x + 0.5*width, yS['Annualized'], width, label='SPY', color=colors[2])
+    plt.bar(x + 1.5*width, yP['Annualized'], width, label='Pairs', color='k')
     plt.axhline(y=0, color='black', linewidth=0.8, linestyle='--')
     plt.xticks(x[::2], years[::2])             # replace numeric x with year labels, showing every second tick to avoid overlapping
     plt.grid(axis='y', alpha=0.3)
